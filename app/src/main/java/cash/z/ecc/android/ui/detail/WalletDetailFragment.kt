0437995ce74c8fd86b110f24aaebddfbb2bbe60a
Kevin Gorham <kevin.gorham@electriccoin.co>
2019-12-18 12:58:27 -0500

    Show message to make it clear that feedback is not implemented.

--- app/src/main/java/cash/z/ecc/android/ui/detail/WalletDetailFragment.kt
+++ app/src/main/java/cash/z/ecc/android/ui/detail/WalletDetailFragment.kt
@@ -4,20 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import androidx.lifecycle.lifecycleScope
 import cash.z.ecc.android.R
 import cash.z.ecc.android.databinding.FragmentDetailBinding
 import cash.z.ecc.android.di.annotation.FragmentScope
-import cash.z.ecc.android.ext.clicks
 import cash.z.ecc.android.ext.onClick
 import cash.z.ecc.android.ext.onClickNavUp
 import cash.z.ecc.android.feedback.FeedbackFile
 import cash.z.ecc.android.ui.base.BaseFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import okio.Okio
 
 
@@ -29,19 +24,23 @@ class WalletDetailFragment : BaseFragment<FragmentDetailBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.backButtonHitArea.onClickNavUp()
+
+        onClick(binding.buttonFeedback) {
+            onSendFeedback()
+        }
         onClick(binding.buttonLogs) {
-            onViewFeedback()
+            onViewLogs()
         }
         onClick(binding.buttonBackup, 1L) {
             onBackupWallet()
         }
     }
 
-    private fun onBackupWallet() {
-        mainActivity?.navController?.navigate(R.id.action_nav_detail_to_backup_wallet)
+    private fun onSendFeedback() {
+        mainActivity?.showSnackbar("Feedback not yet implemented.")
     }
 
-    private fun onViewFeedback() {
+    private fun onViewLogs() {
         loadLogFileAsText().let { logText ->
             if (logText == null) {
                 mainActivity?.showSnackbar("Log file not found!")
@@ -56,6 +55,10 @@ class WalletDetailFragment : BaseFragment<FragmentDetailBinding>() {
                 startActivity(shareIntent)
             }
         }
+    }
+
+    private fun onBackupWallet() {
+        mainActivity?.navController?.navigate(R.id.action_nav_detail_to_backup_wallet)
     }
 
     private fun loadLogFileAsText(): String? {
