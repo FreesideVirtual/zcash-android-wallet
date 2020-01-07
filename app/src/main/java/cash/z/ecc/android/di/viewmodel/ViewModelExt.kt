1937c19a14ad386d52e218d33834dd4955b068ff
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-01-07 01:43:15 -0500

    Improved viewmodel logic for #39.
    - added separate factories for VMs that require the synchronizer and those that do not
    - witched to using the factories directly, instead of creating providers, similar to how Google does it in their generic extension functions but removed the need to inject the factory

--- app/src/main/java/cash/z/ecc/android/di/viewmodel/ViewModelExt.kt
+++ app/src/main/java/cash/z/ecc/android/di/viewmodel/ViewModelExt.kt
@@ -2,35 +2,30 @@ package cash.z.ecc.android.di.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import cash.z.ecc.android.ui.MainActivity
 import cash.z.ecc.android.ui.base.BaseFragment
 
-
-inline fun <reified VM : ViewModel> MainActivity.viewModel() = object : Lazy<VM> {
-    val cached: VM? = null
-    override fun isInitialized(): Boolean = cached != null
-    override val value: VM get() {
-        return cached ?: component.viewModels()[VM::class.java]
-    }
-}
 
 inline fun <reified VM : ViewModel> BaseFragment<*>.viewModel() = object : Lazy<VM> {
     val cached: VM? = null
     override fun isInitialized(): Boolean = cached != null
-    override val value: VM get() {
-        return if (cached != null) cached else {
-            val factory =  mainActivity?.component?.viewModelFactory()
-                ?: throw IllegalStateException("Error: mainActivity should not be null by the time the ${VM::class.java.simpleName} viewmodel is lazily accessed!")
-            ViewModelProvider(this@viewModel, factory)[VM::class.java]
-        }
-    }
+    override val value: VM
+        get() = cached
+            ?: ViewModelProvider(this@viewModel, scopedFactory<VM>())[VM::class.java]
 }
 
-inline fun <reified VM : ViewModel> BaseFragment<*>.activityViewModel() = object : Lazy<VM> {
+inline fun <reified VM : ViewModel> BaseFragment<*>.activityViewModel(isSynchronizerScope: Boolean = true) = object : Lazy<VM> {
     val cached: VM? = null
     override fun isInitialized(): Boolean = cached != null
-    override val value: VM get() {
-        return cached ?: mainActivity?.component?.viewModels()?.get(VM::class.java)
-        ?: throw IllegalStateException("Error: mainActivity should not be null by the time the ${VM::class.java.simpleName} activityViewModel is lazily accessed!")
-    }
+    override val value: VM
+        get() {
+            return cached
+                ?: scopedFactory<VM>(isSynchronizerScope)?.let { factory ->
+                    ViewModelProvider(this@activityViewModel.mainActivity!!, factory)[VM::class.java]
+                }
+        }
+}
+
+inline fun <reified VM : ViewModel> BaseFragment<*>.scopedFactory(isSynchronizerScope: Boolean = true): ViewModelProvider.Factory {
+    val factory = if (isSynchronizerScope) mainActivity?.synchronizerComponent?.viewModelFactory() else mainActivity?.component?.viewModelFactory()
+    return factory ?: throw IllegalStateException("Error: mainActivity should not be null by the time the ${VM::class.java.simpleName} viewmodel is lazily accessed!")
 }
