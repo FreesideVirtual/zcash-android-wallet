9b756d60da181c28129895d287d6d4f091cd4eab
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-02-12 08:03:14 -0500

    Updated build and dependencies.

--- buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Dependencies.kt
@@ -3,7 +3,7 @@ package cash.z.ecc.android
 object Deps {
     // For use in the top-level build.gradle which gives an error when provided
     // `Deps.Kotlin.version` directly
-    const val kotlinVersion = "1.3.60"
+    const val kotlinVersion = "1.3.61"
 
     const val compileSdkVersion = 29
     const val buildToolsVersion = "29.0.2"
@@ -37,7 +37,7 @@ object Deps {
         const val INJECT =                        "javax.inject:javax.inject:1"
     }
     object Kotlin :             Version(kotlinVersion) {
-        val STDLIB =                        "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
+        val STDLIB =                        "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         object Coroutines :     Version("1.3.2") {
             val ANDROID =                   "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
             val CORE =                      "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
